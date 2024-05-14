8839efc758425710373384789dd4f76cac2ef22b
Jhon Bohorquez <jhon@btilab.co>
2024-05-14 15:52:56 -0500

    refactor/add funcion and docs

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 # Importamos las bibliotecas necesarias
 import time
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -10,6 +11,104 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+
+BASE_URL = 'http://localhost:8000'
+TOKEN = ''
+cache =[]
+
+
+
+def get_token():
+    global TOKEN
+    auth_url = f"{BASE_URL}/user/login"
+    payload = {
+        "username": "Jhon",
+        "password": "1234"
+    }
+
+    # Obtener el token
+    auth_response = requests.post(auth_url, json=payload)
+
+    if auth_response.status_code == 200:
+        # La autenticación fue exitosa
+        token = auth_response.json().get("data")
+        if token:
+            TOKEN = token 
+        else:
+            print("Error: No se recibió el token de autenticación.")
+            exit()
+    else:
+        print(f"Error en la autenticación: {auth_response.status_code}")
+        exit()
+
+
+
+def get_data_db():
+    global TOKEN
+    global cache
+    get_token()
+
+    api_url = f"{BASE_URL}/casos"
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
+    # Realizar una solicitud GET con el token de autenticación
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        # La solicitud fue exitosa
+        data = response.json()  # Parsear la respuesta JSON
+        cache = [item['no_causa'] for item in data['data']]
+        print(data)
+    else:
+        # La solicitud falló
+        print(f"Error: {response.status_code}")
+
+
+
+def save_data(no_causa, fecha, hora, tipificacion, contenido):
+
+    global TOKEN
+    api_url = f"{BASE_URL}/casos"
+
+
+    print(no_causa)
+    print(fecha)
+    print(hora)
+    print(tipificacion)
+    print(contenido)
+
+    # Datos a enviar en la solicitud POST
+    payload = {
+        "no_causa": no_causa,
+        "fecha": fecha,
+        "hora": hora,
+        "tipificacion": tipificacion,
+        "contenido": contenido
+    }
+
+    # Encabezados de la solicitud
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    # Realizar la solicitud POST
+    response = requests.post(api_url, headers=headers, json=payload)
+
+    # Verificar el código de estado de la respuesta
+    if response.status_code == 201:
+        # La solicitud fue exitosa
+        print("Caso creado exitosamente.")
+        print("Respuesta del servidor:", response.json())
+    else:
+        # La solicitud falló
+        print(f"Error: {response.status_code}")
+        print("Detalle del error:", response.text)
+
+
+
+
 
 
 def crear_ventana(main):
@@ -26,7 +125,7 @@ def crear_ventana(main):
 
 
 def ciclo_buscar(driver, id, pestana):
-
+    global cache
 
     search = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "texto"))  # Busca el campo por su 'id'
@@ -52,11 +151,16 @@ def ciclo_buscar(driver, id, pestana):
             WebDriverWait(driver, 30).until(
                 EC.invisibility_of_element_located((By.XPATH, xpath_to_wait_for))
             )
+
+            boton_xpath = '/html/body/app-root/app-expel-listado-coincidencias/expel-sidenav/mat-sidenav-container/mat-sidenav-content/section/section[2]/mat-paginator/div/div/div[2]/button[2]'
+            boton = WebDriverWait(driver, 2).until(
+                EC.presence_of_element_located((By.XPATH, boton_xpath))
+            )
         except:
-            ciclos = False
-            no_fund = True
+            no_fund = False
             search.clear()
             break
+
 
         for i in range(1,10):
             try: 
@@ -76,13 +180,19 @@ def ciclo_buscar(driver, id, pestana):
                 no_causa = driver.find_element(By.XPATH, no_causa)
                 
                 print(fecha.text)
+                date = fecha.text.split(' ')
                 print(tipo.text)
-                print(no_causa.text)
+                nuemro = no_causa.text
+                print(nuemro)
                 print('............')
                 print(contenido.text)
+                print('............')
+                print(cache)
+                print('............')
 
-                if pestana: abrir_otra_pestana(driver, no_causa.text)
-
+                if pestana and nuemro not in cache: abrir_otra_pestana(driver, nuemro)
+                if nuemro not in cache: cache.append(nuemro)
+                save_data(nuemro, date[0], date[1], tipo.text, contenido.text)
                 print('------------')                 
                 time_await = 2
 
@@ -92,10 +202,7 @@ def ciclo_buscar(driver, id, pestana):
                 break                 
 
         time_await = 10
-        boton_xpath = '/html/body/app-root/app-expel-listado-coincidencias/expel-sidenav/mat-sidenav-container/mat-sidenav-content/section/section[2]/mat-paginator/div/div/div[2]/button[2]'
-        boton = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, boton_xpath))
-        )
+
         # print("HTML del elemento: ", boton.get_attribute("innerHTML"))
         try:
             valor_disabled = boton.get_attribute("disabled")
@@ -117,9 +224,6 @@ def ciclo_buscar(driver, id, pestana):
 
 
 
-
-
-
 def abrir_otra_pestana(driver, id):
 
     try:
@@ -132,8 +236,6 @@ def abrir_otra_pestana(driver, id):
         driver.quit()
 
 
-
-
 def main():
     driver = crear_ventana(True)
     driver.switch_to.window(driver.window_handles[0])
@@ -142,8 +244,9 @@ def main():
         # Abrimos YouTube
         driver.get("https://procesosjudiciales.funcionjudicial.gob.ec/busqueda")
         WebDriverWait(driver, 10) 
-        lista = ['0968599020001','0992339411001','1791251237001','0968599020001']
+        lista = ['13283202301388G','0968599020001','0992339411001','1791251237001','0968599020001']
         for t in lista:
+            print(t)
             ciclo_buscar(driver, t, True)
 
 
@@ -152,9 +255,11 @@ def main():
 
 
         time.sleep(5)
-    finally:
-        # Cerramos el controlador para evitar fugas de memoria
-        driver.quit()
+    except:
+        print('Error en el proceso')
+    
+    driver.quit()
 
 
+get_data_db()
 main()
